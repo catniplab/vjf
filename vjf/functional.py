32c8a2b486eb9ee63d609cac1ae12e797183c760
yuan <yuanz271@gmail.com>
2021-07-21 10:01:00 -0400

    add RBF functional

---
+++ vjf/functional.py
@@ -0,0 +1,30 @@
+import torch
+from torch import Tensor, cdist
+
+from vjf.module import RBFN
+
+
+def rbf(x: Tensor, c: Tensor, w: Tensor) -> Tensor:
+    """
+    Radial basis functions
+    :param x: input, (batch, dim)
+    :param c: centroids, (basis, dim)
+    :param w: width (scale), (basis)
+    :return:
+    """
+    d = cdist(x, c)  # ||x - c||, (batch, basis)
+    d /= w  # ||x - c||/w
+    return torch.exp(-.5 * d.pow(2))
+
+
+def test_rbf():
+    N, D, B = 100, 5, 10
+    x = torch.randn(N, D)
+    c = torch.randn(B, D)
+    w = torch.rand(B)
+    r1 = rbf(x, c, w)
+
+    f = RBFN(D, B, c, w[:, None].log())
+    r2 = f(x)
+
+    assert torch.allclose(r1, r2)
