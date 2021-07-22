c09556ec47cfb8c3e4b8ced51a092dedc3ba1071
yuan <yuanz271@gmail.com>
2021-07-21 21:36:31 -0400

    add bayesian linear regression and rbf feature

--- vjf/module.py
+++ vjf/module.py
@@ -1,7 +1,9 @@
 import math
 import torch
-from torch import nn
-from torch.nn import Parameter
+from torch import nn, Tensor
+from torch.nn import Parameter, Module, functional
+
+from .functional import rbf
 
 
 class RBFN(nn.Module):
@@ -37,46 +39,72 @@ class RBFN(nn.Module):
         return torch.exp(-0.5 * torch.sum((x * iw - c * iw) ** 2, dim=-1))
 
 
-class SGP(nn.Module):
-    def __init__(self, in_features, out_features, inducing_points, mean, cov):
+class RBF(Module):
+    """Radial basis functions"""
+    def __init__(self, n_dim, n_basis):
         super().__init__()
+        self.n_basis = n_basis
+        self.register_parameter('centroid', Parameter(torch.rand(n_basis, n_dim) - .5, requires_grad=False))
+        self.register_parameter('logwidth', Parameter(torch.zeros(n_basis), requires_grad=False))
 
-    def forward(self, *input):
-        pass
+    @property
+    def n_feature(self):
+        return self.n_basis
+
+    def forward(self, x):
+        return rbf(x, self.centroid, self.logwidth.exp())
 
 
-class IGRU(nn.Module):
-    def __init__(self, in_features, hidden, mapping):
+class bLinReg(Module):
+    """Bayesian linear regression"""
+    def __init__(self, feature: Module, n_output: int, logvar: float):
         super().__init__()
+        self.feature = feature
+        self.n_output = n_output
+        # self.bias = torch.zeros(n_outputs)
+        self.w_mean = torch.zeros(self.feature.n_feature, n_output)
+        self.w_precision = torch.eye(self.feature.n_feature)
+        # It turns out that the precision is independent of the output, and hence only one matrix is needed.
 
-        # reset gate
-        self.add_module(
-            "i2r", nn.Linear(in_features=in_features, out_features=hidden, bias=False)
-        )
-        self.add_module(
-            "h2r", nn.Linear(in_features=hidden, out_features=hidden, bias=True)
-        )
-        # update gate
-        self.add_module(
-            "i2u", nn.Linear(in_features=in_features, out_features=hidden, bias=False)
-        )
-        self.add_module(
-            "h2u", nn.Linear(in_features=hidden, out_features=hidden, bias=True)
-        )
-        # hidden state
-        self.add_module(
-            "i2g", nn.Linear(in_features=in_features, out_features=mapping, bias=False)
-        )
-        self.add_module(
-            "h2g", nn.Linear(in_features=hidden, out_features=mapping, bias=True)
-        )
-        self.add_module(
-            "g2h", nn.Linear(in_features=mapping, out_features=hidden, bias=False)
-        )
+        self.register_parameter('logvar', Parameter(torch.tensor(logvar)))
 
-    def forward(self, hidden, inputs):
-        u = torch.sigmoid(self.i2u(inputs) + self.h2u(hidden))
-        r = torch.sigmoid(self.i2r(inputs) + self.h2r(hidden))
-        g = torch.tanh(self.i2g(inputs) + self.h2g(r * hidden))
-        h = u * hidden + (1 - u) * self.g2h(g)
-        return h
+    def forward(self, x, u=None) -> Tensor:
+        if u is not None:
+            x = torch.cat((x, u), dim=-1)
+        feat = self.feature(x)
+        return functional.linear(feat, self.w_mean.t())  # do we need the intercept?
+
+    def update(self, target, x, u=None):
+        """
+        :param target: (sample, dim)
+        :param x: (sample, dim)
+        :param u: (sample, dim) or None
+        :return:
+        """
+        precision = torch.exp(-self.logvar)
+
+        if u is not None:
+            x = torch.cat((x, u), dim=-1)
+
+        feat = self.feature(x)  # (sample, feature)
+        scaled_feat = precision * feat
+        G = self.w_precision @ self.w_mean + scaled_feat.t() @ target
+        # (feature, feature) (feature, output) + (feature, sample) (sample, output) => (feature, output)
+        self.w_precision = self.w_precision + scaled_feat.t() @ feat
+        # (feature, feature) + (feature, sample) (sample, feature) => (feature, feature)
+        self.w_precision = .5 * (self.w_precision + self.w_precision.t())  # make sure symmetric
+        L = torch.linalg.cholesky(self.w_precision)
+        self.w_mean = G.cholesky_solve(L)  # (feature, feature) (feature, output) => (feature, output)
+
+
+def test_RBF():
+    n_dim, n_basis = 3, 10
+    rbf = RBF(n_dim, n_basis)
+    blr = bLinReg(rbf, n_dim, 0.)
+
+    N = 20
+    x = torch.randn(N, n_dim)
+    y = torch.randn(N, n_dim)
+    blr(x)
+    blr.update(y, x)
+
