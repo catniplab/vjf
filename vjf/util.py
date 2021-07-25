c16836b241cf493b005b6938688a43e352584f39
yuan <yuanz271@gmail.com>
2021-07-25 14:33:38 -0400

    not used any more

--- vjf/util.py
+++ vjf/util.py
@@ -4,13 +4,6 @@ import torch
 from torch import Tensor
 
 
-def complete_shape(a: Tensor) -> Tensor:
-    a = torch.atleast_2d(a)
-    if a.ndim < 3:
-        a = a[None, ...]
-    return a
-
-
 def reparametrize(q: Tuple[Tensor, Tensor]) -> Tensor:
     mean, logvar = q
-    return mean + torch.randn_like(mean) * torch.exp(0.5 * logvar)
+    return mean + torch.randn_like(mean) * torch.exp(.5 * logvar)
