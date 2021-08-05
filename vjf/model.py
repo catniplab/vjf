fix wrong index

vjf/model.py
@@ -269,7 +269,7 @@ class VJF(Module):
                         self.decoder.requires_grad_(False)  # freeze decoder after warm up
                         m = torch.stack([q.mean for q in q_seq])
                         if isinstance(u_, Tensor) and u_.shape[-1] > 0:
-                            u_init = u_[:-1, :].reshape(-1, u_.shape[-1])
+                            u_init = u_[1:, :].reshape(-1, u_.shape[-1])
                         else:
                             u_init = None
                         self.transition.initialize(m[1:].reshape(-1, m.shape[-1]),